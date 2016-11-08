0cbf775632b315373b17270adf0716c1613a63e3
ahs0432 <ahs0432@naver.com>
2016-11-08 20:11:25 +0900

    SApplicatlnfo_2

---
+++ app/src/main/java/com/sungil_i/user/sihschool/activity/SApplicatlnfo_2.java
@@ -0,0 +1,10 @@
+package com.sungil_i.user.sihschool.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by ahs04 on 2016-11-08.
+ */
+
+public class SApplicatlnfo_2 extends Activity {
+}
---
+++ app/src/main/java/com/sungil_i/user/sihschool/datatype/SApplicatlnfoData_2.java
@@ -0,0 +1,8 @@
+package com.sungil_i.user.sihschool.datatype;
+
+/**
+ * Created by ahs04 on 2016-11-08.
+ */
+
+public class SApplicatlnfoData_2 {
+}
