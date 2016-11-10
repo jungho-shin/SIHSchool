c769f728b209846fecff6668ec4c868ef17af9b5
g5hn <g5hn_2d_ck@naver.com>
2016-11-10 19:04:02 +0900

    난 바보다

--- app/src/main/java/com/sungil_i/user/sihschool/activity/SGalleryGridView_2.java
+++ app/src/main/java/com/sungil_i/user/sihschool/activity/SGalleryGridView_2.java
@@ -8,11 +8,3 @@ import android.app.Activity;
 
 public class SGalleryGridView_2 extends Activity {
 }
-/*
-보쿠와 헨타이다!!
-미미짱 다이스키!!
-니코니코니!!
-
-헉헉.. 미쿠짱... 아이시떼이루욧..!!☆
-와... 와타시모.. 아이시떼루 혜성쿤..!!♥♥
- */
