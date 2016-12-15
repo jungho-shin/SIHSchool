10aeb342c8414323cb8180e3784315daa0fadcea
jungho.shin <taos9938@gmail.com>
2016-12-15 11:42:01 +0900

    Add Service.

--- app/src/main/java/com/sungil_i/user/sihschool/service/SConnector.java
+++ app/src/main/java/com/sungil_i/user/sihschool/service/SConnector.java
@@ -1,11 +1,26 @@
 package com.sungil_i.user.sihschool.service;
 
+import android.util.Log;
+
 import com.sungil_i.user.sihschool.datatype.SGalleryData;
 import com.sungil_i.user.sihschool.datatype.SNoticeData;
 import com.sungil_i.user.sihschool.datatype.SScheduleData;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 /**
  * Created by taos9938 on 2016. 12. 14..
@@ -13,14 +28,79 @@ import java.util.ArrayList;
 
 public class SConnector {
 
+    private void request(String strUrl) {
+        Log.d("TEST", "strUrl : " + strUrl);
+        try {
+            URL url = new URL(strUrl);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+//            urlConnection.setDoOutput(false);
+            urlConnection.setDoInput(true);
+            urlConnection.setUseCaches(false);
+            urlConnection.setDefaultUseCaches(false);
+
+            urlConnection.connect();
+
+            InputStream is = urlConnection.getInputStream();
+
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String line;
+            while((line = reader.readLine()) != null) {
+                builder.append(line + "\n");
+                Log.d("TEST", line);
+            }
+
+            JSONObject jsonObject = new JSONObject(builder.toString());
+            Log.d("TEST", "length : " + jsonObject.length());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * 열린마당 > 공지사항 : 석성희.
+     *
+     * @return
+     */
     public ArrayList<SNoticeData> getNotices() {
 
         ArrayList<SNoticeData> notices = new ArrayList<SNoticeData>();
+
+        for(int i = 0; i < 10; i++) {
+
+            SNoticeData data = new SNoticeData();
+            data.setIndex(i);
+            data.setAttachment("test " + i);
+            data.setTitle("Notice title " + i);
+            data.setName("신정호");
+            data.setDate("2016.12.16");
+            data.setHit(i);
+
+            notices.add(data);
+
+        }
+
+        // request("http://45.32.29.112:5000/notices");
 
         return notices;
 
     }
 
+    /**
+     *
+     * 학교생활/학사 > 학사일정 : 안용찬
+     *
+     * @return
+     */
     public ArrayList<SScheduleData> getSchedules() {
 
         ArrayList<SScheduleData> schedules = new ArrayList<SScheduleData>();
@@ -29,15 +109,88 @@ public class SConnector {
 
     }
 
+    /**
+     *
+     * 학교생활/학사 > 급식 > 오늘의 식단 : 양희석
+     *
+     * @return
+     */
     public ArrayList<SNoticeData> getFoods() {
 
         ArrayList<SNoticeData> foods = new ArrayList<SNoticeData>();
+
+        for(int i = 0; i < 10; i++) {
+
+            SNoticeData data = new SNoticeData();
+            data.setIndex(i);
+            data.setAttachment("test " + i);
+            data.setTitle("Schedule title " + i);
+            data.setName("신정호");
+            data.setDate("2016.12.16");
+            data.setHit(i);
+
+            foods.add(data);
+
+        }
 
         return foods;
 
     }
 
-    public void getJobs() {
+    /**
+     *
+     * 가정통신문 :
+     *     학교생활/학사 > 가정통신문 : 박현기
+     *
+     * @return
+     */
+    public ArrayList<SNoticeData> getHomeMail() {
+
+        ArrayList<SNoticeData> homeMails = new ArrayList<SNoticeData>();
+
+        for(int i = 0; i < 10; i++) {
+
+            SNoticeData data = new SNoticeData();
+            data.setIndex(i);
+            data.setAttachment("test " + i);
+            data.setTitle("HomeMail title " + i);
+            data.setName("신정호");
+            data.setDate("2016.12.16");
+            data.setHit(i);
+
+            homeMails.add(data);
+
+        }
+
+        return homeMails;
+
+    }
+
+    /**
+     *
+     * 취업 : 취업 > 취업새소식 : 강찬
+     *
+     * @return
+     */
+    public ArrayList<SNoticeData> getJobs() {
+
+        ArrayList<SNoticeData> jobs = new ArrayList<SNoticeData>();
+
+        for(int i = 0; i < 10; i++) {
+
+            SNoticeData data = new SNoticeData();
+            data.setIndex(i);
+            data.setAttachment("test " + i);
+            data.setTitle("HomeMail title " + i);
+            data.setName("신정호");
+            data.setDate("2016.12.16");
+            data.setHit(i);
+
+            jobs.add(data);
+
+        }
+
+        return jobs;
 
     }
 
