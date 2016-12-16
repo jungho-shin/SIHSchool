2b8844f693d0dffe0cbf0eb30a3aec3889e54b43
jungho.shin <taos9938@gmail.com>
2016-12-16 18:16:50 +0900

    Schedule.

--- app/src/main/java/com/sungil_i/user/sihschool/activity/SSchedule.java
+++ app/src/main/java/com/sungil_i/user/sihschool/activity/SSchedule.java
@@ -1,10 +1,28 @@
 package com.sungil_i.user.sihschool.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import com.sungil_i.user.sihschool.R;
+import com.sungil_i.user.sihschool.widget.Menus;
 
 /**
  * Created by user on 2016-11-08.
  */
 
-public class SSchedule extends Activity {
+public class SSchedule extends CommonActivity {
+
+    CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setCurrentMenu(Menus.MENU_SCHEDULE);
+        setContentView(R.layout.activity_schedule);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+
+    }
 }
