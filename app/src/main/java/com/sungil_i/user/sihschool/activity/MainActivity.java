76b40fe34f049dacc006477a13c8f4d62fb0553a
sunghee <sunghee119z@naver.com>
2017-02-20 14:44:31 +0900

    Rejected commit: Default

--- app/src/main/java/com/sungil_i/user/sihschool/activity/MainActivity.java
+++ app/src/main/java/com/sungil_i/user/sihschool/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sungil_i.user.sihschool.activity;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -9,13 +10,16 @@ import android.os.Bundle;
 
 import com.sungil_i.user.sihschool.R;
 import com.viewpagerindicator.IconPageIndicator;
+import com.viewpagerindicator.TabPageIndicator;
 
 public class MainActivity extends AppCompatActivity {
+    String[] CONTENT = new String[] { "공지", "일정", "급식", "가정"+"\n"+"통신문", "취업", "취업자"+"\n"+"소식" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Fragment[] arrFragment = new Fragment[6];
         arrFragment[0] = new SNoticeFragment();
         arrFragment[1] = new SScheduleFragment();
@@ -24,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
         arrFragment[4] = new SJobFragment();
         arrFragment[3] = new HomeMailFrament();
         ViewPager viewPager = (ViewPager)findViewById(R.id.vp);
-        IconPageIndicator iconPageIndicator = (IconPageIndicator) findViewById(R.id.indicator);
+        TabPageIndicator tabPageIndicator = (TabPageIndicator)findViewById(R.id.indi);
         VpAdapter vpAdapter = new VpAdapter(getSupportFragmentManager(),arrFragment);
         viewPager.setAdapter(vpAdapter);
+        tabPageIndicator.setViewPager(viewPager);
+
+        ;
 
 
 
@@ -46,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return arrFragment.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position];
         }
     }
 }
