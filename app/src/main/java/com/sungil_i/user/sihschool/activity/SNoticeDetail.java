c6004daf712056fab1b9ca02fe76c7d0189ee956
taos9938 <taos9938@gmail.com>
2017-02-21 05:51:58 +0900

    - 내용에 이미지가 있는 경우 정상 출력되지 않는 문제 수정

--- app/src/main/java/com/sungil_i/user/sihschool/activity/SNoticeDetail.java
+++ app/src/main/java/com/sungil_i/user/sihschool/activity/SNoticeDetail.java
@@ -1,11 +1,15 @@
 package com.sungil_i.user.sihschool.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LevelListDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -15,6 +19,12 @@ import com.sungil_i.user.sihschool.R;
 import com.sungil_i.user.sihschool.datatype.SNoticeData;
 import com.sungil_i.user.sihschool.service.SConnector;
 import com.sungil_i.user.sihschool.widget.Menus;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by user on 2016-11-08.
@@ -63,6 +73,60 @@ public class SNoticeDetail extends CommonActivity {
         setCurrentMenu(getCurrentMenu());
     }
 
+    Html.ImageGetter imageGetter = new Html.ImageGetter() {
+        @Override
+        public Drawable getDrawable(String source) {
+            LevelListDrawable d = new LevelListDrawable();
+            Drawable empty = getResources().getDrawable(R.drawable.ic_launcher);
+            d.addLevel(0, 0, empty);
+            d.setBounds(0, 0, empty.getIntrinsicWidth(), empty.getIntrinsicHeight());
+
+            new LoadImage().execute(source, d);
+
+            return d;
+        }
+    };
+
+    class LoadImage extends AsyncTask<Object, Void, Bitmap> {
+        private final String TAG = LoadImage.class.getSimpleName();
+
+        private LevelListDrawable mDrawable;
+
+        @Override
+        protected Bitmap doInBackground(Object... params) {
+            String source = (String) params[0];
+            mDrawable = (LevelListDrawable) params[1];
+            Log.d(TAG, "doInBackground " + source);
+            try {
+                InputStream is = new URL(source).openStream();
+                return BitmapFactory.decodeStream(is);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            Log.d(TAG, "onPostExecute drawable " + mDrawable);
+            Log.d(TAG, "onPostExecute bitmap " + bitmap);
+            if (bitmap != null) {
+                BitmapDrawable d = new BitmapDrawable(bitmap);
+                mDrawable.addLevel(1, 1, d);
+                mDrawable.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
+                mDrawable.setLevel(1);
+                // i don't know yet a better way to refresh TextView
+                // mTv.invalidate() doesn't work as expected
+                CharSequence t = tv_content.getText();
+                tv_content.setText(t);
+            }
+        }
+    }
+
     class NoticeTask extends AsyncTask<Void, Void, SNoticeData> {
 
 
@@ -91,7 +155,7 @@ public class SNoticeDetail extends CommonActivity {
             tv_name.setText(data.getName());
             tv_hits.setText(String.valueOf(data.getHit()));
             tv_date.setText(data.getDate());
-            tv_content.setText(Html.fromHtml(data.getContent()));
+            tv_content.setText(Html.fromHtml(data.getContent(), imageGetter, null));
             tv_content.setMovementMethod(LinkMovementMethod.getInstance());
 
             if(data.getAttachFile1() != null) {
