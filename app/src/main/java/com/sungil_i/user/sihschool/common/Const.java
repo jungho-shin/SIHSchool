c055f12d4ccf9cf211f9cb9130a49831213fccf2
taos9938 <taos9938@gmail.com>
2017-03-01 09:05:32 +0900

    Change connect port to 80

--- app/src/main/java/com/sungil_i/user/sihschool/common/Const.java
+++ app/src/main/java/com/sungil_i/user/sihschool/common/Const.java
@@ -7,7 +7,7 @@ package com.sungil_i.user.sihschool.common;
 public class Const {
 
     public static final String API_DOMAIN = "http://52.79.110.21";
-    public static final String API_PORT = ":5000";
+    public static final String API_PORT = ":80";
 
 
 }
